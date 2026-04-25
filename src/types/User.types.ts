474a2c544cb3cb1b5c3f34f6d957b705f1690f84
rainndev <182114243+rainndev@users.noreply.github.com>
2026-04-25 14:17:57 +0800

    feat: refactor types by moving MenuItem and RandomUser types to a new User.types.ts file

---
+++ src/types/User.types.ts
@@ -0,0 +1,100 @@
+export type MenuItem = {
+  image: string;
+  link: string;
+  title: string;
+  description: string;
+  userDetails: {
+    gender: string;
+    email: string;
+    phone: string;
+    cell: string;
+    nationality: string;
+    age: number;
+    birthDate: string;
+    registeredDate: string;
+    username: string;
+    uuid: string;
+    idName: string;
+    idValue: string | null;
+    location: {
+      street: string;
+      city: string;
+      state: string;
+      country: string;
+      postcode: number | string;
+      latitude: string;
+      longitude: string;
+      timezoneOffset: string;
+      timezoneDescription: string;
+    };
+  };
+};
+
+export type RandomUser = {
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: number | string;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+  email: string;
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  };
+  dob: {
+    date: string;
+    age: number;
+  };
+  registered: {
+    date: string;
+    age: number;
+  };
+  phone: string;
+  cell: string;
+  id: {
+    name: string;
+    value: string | null;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
+};
+
+export type RandomUserInfo = {
+  seed: string;
+  results: number;
+  page: number;
+  version: string;
+};
+
+export type RandomUserResponse = {
+  results: RandomUser[];
+  info: RandomUserInfo;
+};
